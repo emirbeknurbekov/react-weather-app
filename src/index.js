0af298aabd8d5e5bad8b7a187ed4cf802e8dd654
emirbeknurbekov <emirbek.nurbekov@alatoo.edu.kg>
2023-12-13 02:41:51 +0600

    update code to deploy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client"; // Import createRoot from react-dom/client
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>
+);
+
 registerServiceWorker();
