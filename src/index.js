51d342014f4f78bb731a885e269135ff7599bc0c
emirbeknurbekov <emirbek.nurbekov@alatoo.edu.kg>
2023-12-13 03:07:16 +0600

    123

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createRoot } from "react-dom/client"; // Import createRoot from react-dom/client
+import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import App from "./App";
@@ -9,11 +9,9 @@ import registerServiceWorker from "./registerServiceWorker";
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 registerServiceWorker();
